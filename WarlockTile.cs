79d9a66d13ed376906e8af681335bdbf6b47e11c
Jamie-Lee Davies <ST10441037@vgconnect.edu.za>
2024-11-05 12:31:33 +0200

    fixed code

--- WarlockTile.cs
+++ WarlockTile.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ACatsTalePOE
 {
-    public class WarlockTile
+    //3Q2.2
+    public class WarlockTile : EnemyTile
     {
         
     }
